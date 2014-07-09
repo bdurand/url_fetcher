43629d80561601f2f4c1efab195336dfaa268e19
Nicolás Hock Isaza <nhocki@gmail.com>
2014-07-09 15:14:52 -0300

    Move to new Rspec's format
    I hate Travis complaining about old `should` stuff.

--- spec/url_fetcher_spec.rb
+++ spec/url_fetcher_spec.rb
@@ -5,56 +5,58 @@ describe UrlFetcher do
   it "should fetch a URL to a temp file" do
     WebMock.stub_request(:get, "http://example.com/test").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("http://example.com/test")
-    url_fetcher.success?.should == true
-    url_fetcher.redirect?.should == false
-    url_fetcher.header("content-length").should == "5"
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
     url_fetcher.body.open
-    url_fetcher.body.read.should == "Hello"
+    expect(url_fetcher.body.read).to eql("Hello")
   end
 
   it "should perform a POST request" do
     WebMock.stub_request(:post, "http://example.com/test").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("http://example.com/test", :method => :post)
-    url_fetcher.success?.should == true
-    url_fetcher.redirect?.should == false
-    url_fetcher.header("content-length").should == "5"
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
     url_fetcher.body.open
-    url_fetcher.body.read.should == "Hello"
+    expect(url_fetcher.body.read).to eql("Hello")
   end
 
   it "should perform a HEAD request" do
     WebMock.stub_request(:head, "http://example.com/test").to_return(:status => 200, :body => nil, :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("http://example.com/test", :method => :head)
-    url_fetcher.success?.should == true
-    url_fetcher.redirect?.should == false
-    url_fetcher.header("content-length").should == "5"
-    url_fetcher.body.should == nil
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
+    expect(url_fetcher.body).to be_nil
   end
 
   it "should work with SSL" do
     WebMock.stub_request(:get, "https://example.com/test").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("https://example.com/test")
-    url_fetcher.success?.should == true
-    url_fetcher.redirect?.should == false
-    url_fetcher.header("content-length").should == "5"
-    url_fetcher.body.open.read.should == "Hello"
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
+    expect(url_fetcher.body.open.read).to eql("Hello")
   end
 
   it "should honor redirects" do
     WebMock.stub_request(:get, "http://example.com/test1").to_return(:status => 301, :headers => {"Location" => "http://example.com/test2"})
     WebMock.stub_request(:get, "http://example.com/test2").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("http://example.com/test1")
-    url_fetcher.success?.should == true
-    url_fetcher.redirect?.should == false
-    url_fetcher.header("content-length").should == "5"
-    url_fetcher.body.open.read.should == "Hello"
+
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
+    expect(url_fetcher.body.open.read).to eql("Hello")
   end
 
   it "should not honor redirects if :follow_redirects == false" do
     WebMock.stub_request(:get, "http://example.com/test1").to_return(:status => 301, :headers => {"Location" => "http://example.com/test2"})
     url_fetcher = UrlFetcher.new("http://example.com/test1", :follow_redirects => false)
-    url_fetcher.success?.should == false
-    url_fetcher.redirect?.should == true
+
+    expect(url_fetcher).not_to be_success
+    expect(url_fetcher).to be_redirect
   end
 
   it "should call a block before each redirect with the new location" do
@@ -65,10 +67,12 @@ describe UrlFetcher do
     url_fetcher = UrlFetcher.new("http://example.com/test1") do |location|
       redirects << location
     end
-    url_fetcher.success?.should == true
-    url_fetcher.body.open
-    url_fetcher.body.read.should == "Hello"
-    redirects.should == ["http://example.com/test2", "http://example.com/test3"]
+
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
+    expect(url_fetcher.body.open.read).to eql("Hello")
+    expect(redirects).to eql(["http://example.com/test2", "http://example.com/test3"])
   end
 
   it "should abort redirecting if a block is given that returns false" do
@@ -80,41 +84,45 @@ describe UrlFetcher do
       redirects << location
       false
     end
-    url_fetcher.success?.should == false
-    url_fetcher.redirect?.should == true
-    url_fetcher.body.should == nil
-    redirects.should == ["http://example.com/test2"]
+
+    expect(url_fetcher).not_to be_success
+    expect(url_fetcher).to be_redirect
+    expect(url_fetcher.body).to be_nil
+    expect(redirects).to eql(["http://example.com/test2"])
   end
 
   it "should raise an error if there is a circular redirect" do
     WebMock.stub_request(:get, "http://example.com/test").to_return(:status => 302, :headers => {"Location" => "http://example.com/test"})
-    lambda{ UrlFetcher.new("http://example.com/test") }.should raise_error(UrlFetcher::CircularRedirect)
+    expect{ UrlFetcher.new("http://example.com/test") }.to raise_error(UrlFetcher::CircularRedirect)
   end
 
   it "should raise an error if there are too many redirects" do
     6.times do |i|
       WebMock.stub_request(:get, "http://example.com/test#{i}").to_return(:status => 302, :headers => {"Location" => "http://example.com/test#{i + 1}"})
     end
-    lambda{ UrlFetcher.new("http://example.com/test0") }.should raise_error(UrlFetcher::TooManyRedirects)
+    expect{ UrlFetcher.new("http://example.com/test0") }.to raise_error(UrlFetcher::TooManyRedirects)
   end
 
   it "should raise an error if an HTTP error is returned" do
     WebMock.stub_request(:get, "http://example.com/test").to_return(:status => 404, :body => "Not Found")
-    lambda{ UrlFetcher.new("http://example.com/test") }.should raise_error(Net::HTTPServerException)
+    expect{ UrlFetcher.new("http://example.com/test") }.to raise_error(Net::HTTPServerException)
   end
 
   it "should not unlink the temp file if asked not to" do
     WebMock.stub_request(:get, "http://example.com/test").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 5})
     url_fetcher = UrlFetcher.new("http://example.com/test", :unlink => false)
-    url_fetcher.success?.should == true
-    url_fetcher.body.open.read.should == "Hello"
-    url_fetcher.body.path.should_not == nil
+
+    expect(url_fetcher).to be_success
+    expect(url_fetcher).not_to be_redirect
+    expect(url_fetcher.header("content-length")).to eql("5")
+    expect(url_fetcher.body.open.read).to eql("Hello")
+    expect(url_fetcher.body.path).not_to be_nil
   end
 
   it "should limit the size of the file downloaded" do
     WebMock.stub_request(:get, "http://example.com/test").to_return(:status => 200, :body => "Hello", :headers => {"Content-Length" => 1001})
-    lambda do
+    expect do
       UrlFetcher.new("http://example.com/test", :max_size => 1000)
-    end.should raise_error
+    end.to raise_error
   end
 end
