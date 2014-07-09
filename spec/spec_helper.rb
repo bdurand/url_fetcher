08629509af2801c89f2d4d147fb9603e72c5cc84
Nicolás Hock Isaza <nhocki@gmail.com>
2014-07-09 15:18:10 -0300

    Remove more Rspec warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'url_fetcher'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
